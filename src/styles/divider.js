b9bd44d1af9e1704e1405c91756d757ea6a6ac2a
Kagan Ugur <kgnugur@gmail.com>
2019-07-24 19:53:23 +0300

    Better clip for desktop

--- src/styles/divider.js
+++ src/styles/divider.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { ParallaxLayer } from 'react-spring/renderprops-addons'
+import breakpoint from 'styled-components-breakpoint'
 
 export const Divider = styled(ParallaxLayer)`
   position: absolute;
@@ -13,4 +14,8 @@ export const Divider = styled(ParallaxLayer)`
 
 export const DividerMiddle = styled(Divider)`
   clip-path: polygon(0 0, 100% 10%, 100% 100%, 0 90%);
+
+  ${breakpoint('desktop')`
+    clip-path: polygon(0 5%, 100% 15%, 100% 95%, 0 85%);
+  `};
 `
