d3d4d48208bb33b90e25084c0082e4e430b4ffb9
Kagan Ugur <kgnugur@gmail.com>
2019-07-24 19:57:17 +0300

    Mobile manifest color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#877DA3`,
-        theme_color: `#877DA3`,
+        background_color: `#E4E0D9`,
+        theme_color: `#E4E0D9`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
